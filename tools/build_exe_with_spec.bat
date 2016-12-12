b0b38af62c4408226096e2d7a056c520a2685598
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-12-12 12:58:02 +0300

    build exe: added the cleanup.

--- tools/build_exe_with_spec.bat
+++ tools/build_exe_with_spec.bat
@@ -3,4 +3,7 @@ SET OUT_DIR=..\.build
 
 SET PKG_NAME=rail_radar_diagnost
 
+rmdir /S /Q %OUT_DIR%\build
+rmdir /S /Q %OUT_DIR%\dist
+
 %PYT_PATH%\Scripts\pyinstaller.exe --distpath %OUT_DIR%\dist --workpath %OUT_DIR%\build -y --clean diagnost.spec
